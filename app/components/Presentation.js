ceefba1bda247d1e0c0bd834edfa300a334c1c65
Agustin-desktop <agustin2051@gmail.com>
2023-11-03 20:38:54 -0300

    Cool

--- app/components/Presentation.js
+++ app/components/Presentation.js
@@ -58,7 +58,7 @@ export const Presentation = () => {
               <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
                 <div className="absolute inset-0 w-3 bg-[#0065B3] transition-all duration-[250ms] ease-out group-hover:w-full"></div>
                 <span className="relative text-black group-hover:text-white">
-                  Ir a los posts
+                  Empezar
                 </span>
               </button>
             </Link>
