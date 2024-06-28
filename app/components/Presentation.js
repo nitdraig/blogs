b4bacfe4bd8d54c4e12695513ea1bca9470b1e13
Agustin-desktop <agustin2051@gmail.com>
2024-06-28 15:37:00 -0300

    blog_14

--- app/components/Presentation.js
+++ app/components/Presentation.js
@@ -1,11 +1,9 @@
 import React from "react";
-
-import { SocialMedias } from "./SocialMedias";
 import Link from "next/link";
 
 export const Presentation = () => {
   return (
-    <section className="pl-10 pr-10 pt-8 pb-10">
+    <section className="lg:px-10 px-2 pt-4 pb-8">
       <div className="mx-auto flex flex-col items-center space-y-8">
         <img
           className="w-28"
@@ -13,47 +11,41 @@ export const Presentation = () => {
         />
       </div>
       <div className="flex flex-col space-y-4 text-center">
-        <h2 className="text-black  pb-8 mx-auto my-2 max-w-md text-2xl font-bold tracking-tight md:text-3xl">
+        <h2 className="text-black  mx-auto   text-2xl font-bold tracking-tight lg:text-3xl">
           Bienveni@ a este espacio de reflexión
         </h2>
-        <div className="mx-auto max-w-md text-gray-900 md:text-lg">
+        <div className="mx-auto  text-gray-900 lg:text-lg text-md ">
           <p>
             Si te encuentras acá, significa que seguramente sabes quién soy.
-          </p>
-          <p>
+            <br />
             Sin embargo daré una breve explicación de porque cree este segmento.
             No soy participe de la autodefinición como método de presentación,
             es decir, prefiero que se me conozca en conversación e interacción
             que en describirme a mí mismo.
-          </p>
-
-          <p>
+            <br />
             Cabe destacar que todo lo aquí presentado es en un 50% de mi
-            percepción de las cosas y solo un 50% de objetividad.
-          </p>
-          <p>
+            percepción de las cosas y solo un 50% de objetividad. <br />
             Espero te sea de utilidad y puedas compartir conmigo cualquier
-            consulta en este camino llamado vida. Los temas que encontrarás en
-            este blog:
+            consulta en este camino llamado vida. <br />{" "}
+            <span className="font-bold">
+              Los temas que encontrarás en este blog:
+            </span>
           </p>
         </div>
-        <div className="text-[1.3em] pt-4 pb-4  font-semibold">
-          <ul>
-            <li>- Programación</li>
-            <li>- Gestión de proyectos</li>
-            <li>- Quality assurance</li>
-            <li>- Actualidad digital</li>
-            <li>- Inteligencia Artificial</li>
-            <li>- Opiniones sobre temas relacionados al mundo IT</li>
-          </ul>
+        <div className="text-md  font-semibold">
+          <p className="mx-auto uppercase">
+            Programación, Gestión de proyectos, Quality assurance, Actualidad
+            digital, Inteligencia Artificial, Opiniones sobre temas relacionados
+            al mundo IT, etc.
+          </p>
         </div>
-        <div>
-          <p className="mx-auto  text-gray-900 md:text-2xl">
+        <div className="-mt-2">
+          <p className=" text-gray-900  text-lg">
             Espero puedas llevarte algo de mí en el mejor de los sentidos.
           </p>
-          <p className="text-2xl">Puedes contactarme por mis redes. ;)</p>
+          <p className="text-md">Puedes contactarme por mis redes. ;)</p>
 
-          <div className="mt-5">
+          <div className="mt-2">
             <Link href="/blog">
               <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
                 <div className="absolute inset-0 w-3 bg-[#0065B3] transition-all duration-[250ms] ease-out group-hover:w-full"></div>
---
+++ blogposts/014_m1_sm1_google_course.md
@@ -0,0 +1,37 @@
+---
+title: 'El Ciclo de Vida de un Proyecto | FGPG Módulo 1, SubMódulo 1 '
+date: '2024-06-24'
+author: "Agustín"
+image: "https://res.cloudinary.com/draig/image/upload/v1719294779/portolio-personal/blog/zwocgc3wmra9ukb4cnag.webp"
+---
+
+
+# "El Ciclo de Vida de un Proyecto: De la Iniciación al Cierre"
+En el módulo de Fundamentos de la gestión de proyectos, aprendimos sobre los aspectos básicos y primordiales de la gestión de proyectos. Las clases, con historias de primera mano de miembros de diferentes equipos de Google, son simples, directas y fáciles de digerir. Aquí te dejo un resumen de los puntos claves sobre el ciclo de vida de un proyecto:
+
+
+## El ciclo de vida de un proyecto se divide en cuatro etapas fundamentales:
+
+*1. Iniciación del proyecto:*
+
+Determinación de objetivos, entregables, recursos y selección del equipo.
+Establecimiento de los criterios de éxito y las expectativas del proyecto.
+
+*2. Planificación:*
+
+Diagramación de un plan detallado, ajustado al presupuesto y recursos disponibles.
+Definición de cronogramas, asignación de tareas y establecimiento de hitos.
+
+*3. Ejecución y seguimiento:*
+
+Supervisión del cumplimiento de las tareas dentro del tiempo determinado.
+Facilitación de herramientas y recursos necesarios para el equipo.
+Comunicación constante con los interesados sobre el progreso.
+
+*4. Cierre del proyecto:*
+
+Entrega y evaluación de los resultados.
+Documentación de lecciones aprendidas y recomendaciones para futuros proyectos.
+Estas etapas son esenciales para garantizar que un proyecto se complete con éxito y dentro de los parámetros establecidos.
+
+***Eso es todo por este submódulo, espero esta lectura te haya sido beneficiosa, y no olvides que el siguiente viernes estará disponible el siguiente resumen de los puntos que considero más importantes del primer módulo de este curso.***
