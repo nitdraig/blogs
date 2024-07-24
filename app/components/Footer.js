03681850a7b0eac54b3dcf181e24e68b395969ac
Agustin-desktop <agustin2051@gmail.com>
2024-07-24 02:41:10 -0300

    Footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -12,7 +12,7 @@ export const Footer = () => {
           <div className="pt-3 pb-3 items-center text-sm  hover:scale-110 text-gray-400 ">
             <a
               className="order-2 md:order-1 mt-8 md:mt-0 "
-              href="https://www.linkedin.com/in/avellaneda-agust%C3%ADn-tns"
+              href="https://www.linkedin.com/in/avellaneda-agustín-tns/"
               target="blank"
             >
               Develop with <span className="text-red-800 text-lg">♥</span> by
