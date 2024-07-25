0fcde3c5d365101c0c6b7adbb61575ea8badc43d
Agustin-desktop <agustin2051@gmail.com>
2024-07-25 00:33:46 -0300

    .env

--- blog-es/app/page.js
+++ blog-es/app/page.js
@@ -4,13 +4,15 @@ import IndexView from "./views/IndexView/IndexView";
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RR6M6KD3ZP" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLEANALYTIC}`}
+      />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-RR6M6KD3ZP');
+         gtag('config', ${process.env.NEXT_PUBLIC_GOOGLEANALYTIC});
                   `}
       </Script>
       <IndexView />
